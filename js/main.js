bd24cc0fe0cc890041b6bdfa5261aa7bb467a330
coloradojay <delarosa.j@gmail.com>
2014-11-24 17:28:49 -0800

    V3.0 Removing breaks from code

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
+
+
+
+
+
 function calc() {
     var bill = Number(document.getElementById('bill').value);
+    // Get the bill from the input box
     var tip = bill * .05;
+    // Calc the tipe at only 5% -- What a Cheapskate!
     var total_bill = bill + tip;
+    // Calc the total bill by adding the bill and the tip
 
     document.getElementById("tip").innerHTML= "$"+Number(tip).toFixed(2);
-    document.getElementById("total").innerHTML= "$"+Number(total_bill).toFixed(2);  
-    document.getElementById("gohome").innerHTML= "OUCH!<br />You need to keep saving those pennies.";       
+    // Adding the tip calc into the tip id on the form
+    document.getElementById("total").innerHTML= "$"+Number(total_bill).toFixed(2);
+    // Adding the total amount to be paid (tip + bill)
+    document.getElementById("gohome").innerHTML= "OUCH!<br />You need to keep saving those pennies.";
+    // Adding message back to the cheapskate
 }
