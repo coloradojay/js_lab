866e587a9399b978836bc953892cfa9ddc6d2bb2
coloradojay <delarosa.j@gmail.com>
2014-11-24 17:04:11 -0800

    V2.0 adding text once the button is placed.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 function calc() {
-    var bill = document.getElementById('bill').value;
+    var bill = Number(document.getElementById('bill').value);
     var tip = bill * .05;
-    var total = bill + tip;
+    var total_bill = bill + tip;
 
-    document.getElementById("tip").innerHTML= "$"+(tip).toFixed(2);
-    document.getElementById("total").innerHTML= "$"+(total).toFixed(2);         
+    document.getElementById("tip").innerHTML= "$"+Number(tip).toFixed(2);
+    document.getElementById("total").innerHTML= "$"+Number(total_bill).toFixed(2);  
+    document.getElementById("gohome").innerHTML= "OUCH!<br />You need to keep saving those pennies.";       
 }
